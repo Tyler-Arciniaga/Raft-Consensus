277ae9221309be7c708187c48537ffbff22aa4db
Tyler Arciniaga <tylerarc@umich.edu>
2026-04-02 18:20:31 -0400

    chore: stub out node RPC funcs, create state transition logger functions

--- raft_node.cpp
+++ raft_node.cpp
@@ -1,8 +1,32 @@
 #include <cstdint>
+#include <iostream>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
 enum NodeState { Follower = 0, Candidate, Leader };
+std::string print_state(NodeState state) {
+  std::string state_string;
+  switch (state) {
+  case NodeState::Follower:
+    state_string = "Follower";
+    break;
+  case NodeState::Candidate:
+    state_string = "Candidate";
+    break;
+  case NodeState::Leader:
+    state_string = "Leader";
+    break;
+  }
+
+  return state_string;
+}
+
+void print_switch_state_statement(uint64_t nodeID, NodeState oldState,
+                                  NodeState newState) {
+  std::cout << "Switching node " << nodeID << " from " << print_state(oldState)
+            << " to " << print_state(newState) << "\n";
+}
 
 enum ServerAction { Add = 0, Remove };
 
@@ -13,9 +37,18 @@ struct LogEntry {
   uint64_t termReceived;
 };
 
+struct AppendEntriesArgs {};
+struct AppendEntriesReply {};
+
+struct RequestVoteArgs {};
+struct RequestVoteReply {};
+
 class RaftNode {
 public:
   RaftNode(size_t nodeID);
+
+  AppendEntriesReply AppendEntries(AppendEntriesArgs args);
+  RequestVoteReply RequestVote(RequestVoteReply args);
 
 private:
   NodeState state;
@@ -38,8 +71,27 @@ private:
   std::vector<uint32_t>
       matchIndex; // index of highest log entry known to be replicated for each
                   // server (used by leader)
+
+  void SwitchStateToFollower();
+  void SwitchStateToCandidate();
+  void SwitchStateToLeader();
 };
 
 RaftNode::RaftNode(size_t nodeID)
     : nodeID(nodeID), state(NodeState::Follower), currentTerm(0),
       commitIndex(0), lastApplied(0) {};
+
+void RaftNode::SwitchStateToFollower() {
+  print_switch_state_statement(nodeID, state, NodeState::Follower);
+  state = NodeState::Follower;
+}
+
+void RaftNode::SwitchStateToCandidate() {
+  print_switch_state_statement(nodeID, state, NodeState::Candidate);
+  state = NodeState::Candidate;
+}
+
+void RaftNode::SwitchStateToLeader() {
+  print_switch_state_statement(nodeID, state, NodeState::Leader);
+  state = NodeState::Leader;
+}
